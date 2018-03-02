4d50c39c3cbdd7f44cd4275c68a8d37b0cc0e66d
WilfredoIvanhoe <jordan.965@hotmail.com>
2018-03-02 05:49:58 -0600

    New Queries and design

--- Consultar.php
+++ Consultar.php
@@ -14,8 +14,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT Nombre,CodigoDeBarras,idPresentacion,idViaDeAdmin,idDosis,PrecioProveedor,PrecioPublico,Concentracion,Cantidad,Medida,idConsumidor,FechaCad,Lote
-        FROM producto WHERE CodigoDeBarras ='".$_POST['codigo']."'";      
+$aliases= "CodigoDeBarras, producto.Nombre AS Nombre, FechaCad, Lote, PrecioPublico, PrecioProveedor, Cantidad, Concentracion,
+		   dosis.Nombre AS nombreDosis, presentacion.Nombre AS nombrePres, viadeadmin.Nombre AS nombreVia, 
+		   consumidor.Nombre AS nombreCons";
+
+$sql = "SELECT ".$aliases." FROM producto 
+		LEFT JOIN (presentacion, dosis, consumidor, viadeadmin) 
+		ON (producto.idPresentacion = presentacion.idPresentacion AND producto.idDosis = dosis.idDosis AND producto.idConsumidor = consumidor.idConsumidor AND producto.idViaDeAdmin = viadeadmin.idViaDeAdmin) 
+		WHERE producto.CodigoDeBarras ='".$_POST['codigo']."'";     
 $result = $conn->query($sql);
 
 if ( $result->num_rows > 0) {
@@ -25,10 +31,10 @@ if ( $result->num_rows > 0) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$sql = "SELECT Nombre,CodigoDeBarras,idPresentacion,idViaDeAdmin,idDosis,PrecioProveedor,PrecioPublico,Concentracion,Cantidad,Medida,idConsumidor,FechaCad,Lote
+/**$sql = "SELECT Nombre,CodigoDeBarras,idPresentacion,idViaDeAdmin,idDosis,PrecioProveedor,PrecioPublico,Concentracion,Cantidad,Medida,idConsumidor,FechaCad,Lote
         FROM producto WHERE CodigoDeBarras ='".$_POST['codigo']."'";      
 $result = $conn->query($sql);
-
+**/
 $conn->close();
 
 
@@ -54,120 +60,85 @@ $conn->close();
 					<img src="Logo.png" width="350" height="180" alt="Logo">
 				</div>
 			</div>
-
+		</div>
+		<div>
 			<!-- TABLE -->
 			<div class="row">
-				<div class="col s12 m12">
+				<div class="col s12 m12 l12">
 					<table class="bordered">
 						<thead>
 							<tr>
-								<th></th>
-								<th>Información del producto</th>
+								<th>Código de Barras</th>
+								<th>Nombre</th>
+								<th>Fecha de caducidad</th>
+								<th>Lote</th>
+								<th>Precio al público</th>
+								<th>Precio unitario</th>
+								<th>Cantidad</th>
+								<th>Concentración</th>
+								<th>Dosis</th>
+								<th>Presentación</th>
+								<th>Vía de Administración</th>
+								<th>Medicamento para</th>
 							</tr>
 						</thead>
-						<tr>
-							<td>	
-								<b>Código de Barras</b>
-							</td>
-							<td>	
-								<?php echo $row['CodigoDeBarras']?>
-							</td>
-						</tr>
-						<tr>
-							<td>	
-								<b>Nombre</b>
-							</td>
-							<td>	
-								<?php echo $row['Nombre']?>
-							</td>
-						</tr>
-						<tr>
-							<td valign="top">	
-								<b>Ingrediente Activo</b>
-							</td>
-							<td>	
-								
-							</td>
-						</tr>
-						<tr>
-							<td>	
-								<b>Fecha de caducidad</b>
-							</td>
-							<td>	
-								<?php echo $row['FechaCad']?>
-							</td>
-						</tr>
-						<tr>
-							<td>	
-								<b>Lote</b>
-							</td>
-							<td>	
-								<?php echo $row['Lote']?>
-							</td>
-						</tr>
-						<tr>
-							<td>	
-								<b>Precio al público</b>
-							</td>
-							<td>	
-								<?php echo $row['PrecioPublico']?>
-							</td>
-						</tr>
-						<tr>
-							<td>	
-								<b>Precio unitario</b>
-							</td>
-							<td>	
-								<?php echo $row['PrecioProveedor']?>
-							</td>
-						</tr>
-						<tr>
-							<td>	
-								<b>Cantidad</b>
-							</td>
-							<td>	
-								<?php echo $row['Cantidad']?>
-							</td>
-						</tr>
-						<tr>
-							<td>	
-								<b>Concentración</b>
-							</td>
-							<td>	
-								<?php echo $row['Concentracion']?>
-							</td>
-						</tr>
-						<tr>
-							<td>	
-								<b>Vía de administración</b>
-							</td>
-							<td>	
-								<?php echo $row['idViaDeAdmin']?>
-							</td>
-						</tr>
-						<tr>
-							<td>	
-								<b>Medicamento para</b>
-							</td>
-							<td>	
-								<?php echo $row['idConsumidor']?>
-							</td>
-						</tr>
+						<tbody>
+							<tr>
+								<td>	
+									<?php echo $row['CodigoDeBarras']?>
+								</td>
+								<td>	
+									<?php echo $row['Nombre']?>
+								</td>
+								<td>	
+									<?php echo $row['FechaCad']?>
+								</td>
+								<td>	
+									<?php echo $row['Lote']?>
+								</td>
+								<td>	
+									<?php echo $row['PrecioPublico']?>
+								</td>
+								<td>	
+									<?php echo $row['PrecioProveedor']?>
+								</td>
+								<td>	
+									<?php echo $row['Cantidad']?>
+								</td>
+								<td>	
+									<?php echo $row['Concentracion']?>
+								</td>
+								<td>	
+									<?php echo $row['nombreDosis']?>
+								</td>
+								<td>	
+									<?php echo $row['nombrePres']?>
+								</td>
+								<td>	
+									<?php echo $row['nombreVia']?>
+								</td>
+								<td>	
+									<?php echo $row['nombreCons']?>
+								</td>
+							</tr>
+						</tbody>
 					</table>	
 				</div>
 			</div>	
-			<div class="row light-blue lighten-2">		
-				<div class="input-field col s12 m6">	
-					<a class="waves-effect waves-light btn" href='Principal.html'>
-						Regresar <i class="material-icons right">arrow_back	</i>
-					</a>
+			<div class="container">
+				<div class="row light-blue lighten-2">		
+					<div class="input-field col s12 m6">	
+						<a class="waves-effect waves-light btn" href='Principal.html'>
+							Regresar <i class="material-icons right">arrow_back	</i>
+						</a>
+					</div>
+					<div class="input-field col s12 m6 right-align">
+						<a class="waves-effect waves-light btn" href='Login.html'>
+							Salir <i class="material-icons right">close</i>
+						</a>
+					</div>	
 				</div>
-				<div class="input-field col s12 m6 right-align">
-					<a class="waves-effect waves-light btn" href='Login.html'>
-						Salir <i class="material-icons right">close</i>
-					</a>
-				</div>	
-			</div>
+			</div>	
 		</div>
 	</body>
 </html>
